417095fb8a294b264600182ceb34501505aad2d8
simon <simon.hobbs@electrooptical.net>
2019-03-27 15:17:32 -0400

    option to select serial port added

--- ISPProgrammer/LPC84x.py
+++ ISPProgrammer/LPC84x.py
@@ -6,6 +6,7 @@ class LPC84x(NXPChip):
         'LPC845', 
         'LPC844',         
     )
+    MAXBAUDRATE = 57600
     CrystalFrequency = 30000#khz == 30MHz
     PartIDs = (
         0x00008441,
