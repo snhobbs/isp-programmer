0aa42d0cc26fc963050fda3872fdaa635286d6bb
Simon <simon.hobbs@electrooptical.net>
2020-01-14 09:19:13 -0500

    changed default clock speeds

--- ISPProgrammer/LPC84x.py
+++ ISPProgrammer/LPC84x.py
@@ -1,18 +1,19 @@
-from . import NXPChip 
+from . import NXPChip
 KILOBYTE = 1024
 class LPC84x(NXPChip):
     ChipName = "LPC84x"
     Family = (
-        'LPC845', 
-        'LPC844',         
+        'LPC845',
+        'LPC844',
     )
-    #MAXBAUDRATE = 460800
-    #MAXBAUDRATE = 115200
-    MAXBAUDRATE = 230400
+    MAXBAUDRATE = 115200
+
+    #MAXBAUDRATE = 230400
+    #MAXBAUDRATE = 9600
 
     #MAXBAUDRATE = 57600
-    CrystalFrequency = 30000#khz == 30MHz
-    #CrystalFrequency = 12000#khz == 30MHz
+    #CrystalFrequency = 30000#khz == 30MHz
+    CrystalFrequency = 12000#khz == 30MHz
     #CrystalFrequency = 15000#khz == 30MHz
     PartIDs = (
         0x00008441,
@@ -28,7 +29,7 @@ class LPC84x(NXPChip):
     RAMSize = 16*KILOBYTE
     RAMRange = (0x10000000, 0x10004000)
     FlashRange = (0x0, SectorCount*NXPChip.PageSizeBytes*NXPChip.SectorSizePages-1)
-    RAMStartWrite = 0x10000800#the ISP stack starts 
+    RAMStartWrite = 0x10000800#the ISP stack starts
     #StartExecution = 0x0F000000
 
     assert(FlashRange[1] == 0x00010000-1)
