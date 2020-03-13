87fc4ed079f563d30c4a02740c57d06cdc908de6
Simon <simon.hobbs@electrooptical.net>
2020-03-13 18:35:00 -0400

    release incriment

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages
 
 setup(name='ISPProgrammer',
-    version='0.0.2',
+    version='1.0.0',
     description='',
     url='',
     author='ElectroOptical Innovations, LLC.',
