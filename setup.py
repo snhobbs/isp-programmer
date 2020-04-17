f664d095c7022abab179b3fff1aa831935404b2c
Simon <simon.hobbs@electrooptical.net>
2020-04-17 13:36:43 -0400

    updating setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,26 @@
 #!/usr/bin/env python3
 from setuptools import setup, find_packages
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
 setup(name='ISPProgrammer',
-    version='1.0.0',
-    description='',
-    url='',
-    author='ElectroOptical Innovations, LLC.',
+    version='1.0.1',
+    description='NXP ISP Cortex-M programming tool',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/snhobbs/NXPISP',
+    author='Simon Hobbs',
     author_email='simon.hobbs@electrooptical.net',
-    license='BSD',
+    license='MIT',
     packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
     install_requires=[
         'click',
         'pyserial',
