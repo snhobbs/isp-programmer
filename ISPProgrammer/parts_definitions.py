558ff6f0d9a29243b67bc4ad94963e577bd6c788
simon <simon.hobbs@electrooptical.net>
2022-10-24 14:23:26 -0400

    adding datafram abstraction around file format

--- ISPProgrammer/parts_definitions.py
+++ ISPProgrammer/parts_definitions.py
@@ -1,17 +1,58 @@
-def ReadChipFile(fname: str) -> list:
-    data = []
+'''
+Parser for the lpctools file, read into a data frame that is consitant with other formats
+'''
+import pandas
+
+column_names = [
+    "SectorCount",
+    "RAMSize",
+    "RAMStart",
+    "RAMRange",
+    "FlashStart",
+    "FlashSize",
+    "FlashRange",
+    "RAMBufferOffset",
+    "RAMStartWrite",
+]
+
+
+def ReadChipFile(fname: str) -> pandas.DataFrame:
+    lpc_tools_column_locations = {
+        "part id": 0,
+        "name": 1
+        "FlashStart": 2,
+        "FlashSize": 3,
+        "SectorCount": 4,
+        "ResetVectorOffset": 5
+        "RAMStart": 6,
+        "RAMSize": 7,
+        "RAMBufferOffset": 8,
+        "RAMBufferSize": 9,
+        "UU Encode": 10,
+    }
+    df_dict = {}
+    for column in column_names:
+        df_dict[column] = []
+
     with open(fname, 'r') as f:
         for line in f:
             if not line.strip() or line.strip()[0] == '#':
                 continue
-            data.append(line.split(","))
-    return data
+            split_line = line.strip().split(',')
+            for column in lpc_tools_column_locations:
+                df_dict[column].append(split_line[lpc_tools_column_locations[column]])
+
+        df = pandas.DataFrame(df_dict)
+        df["RAMRange"] = (df["RAMStart"], df["RAMStart"] + df["RAMSize"] - 1)
+        df["FlashRange"] = (df["FlashStart"], df["FlashStart"] + df["FlashSize"] - 1)
+        df["RAMStartWrite"] = df["RAMStart"] + df["RAMBufferOffset"]
+    return df
 
 
 def GetPartDescriptorLine(fname: str, partid: int) -> list:
     entries = ReadChipFile(fname)
-    for entry in entries:
-        if partid == int(entry[0], 0):
+    for _, entry in entries.iterrows():
+        if partid == int(entry["part id"], 0):
             return entry
     raise UserWarning(f"PartId {partid : 0x%x} not found in {fname}")
 
@@ -20,15 +61,4 @@ def GetPartDescriptor(fname: str, partid: int) -> dict:
     descriptor = GetPartDescriptorLine(fname, partid)
     if descriptor is None:
         raise UserWarning("Warning chip %s not found in file %s"%(hex(partid), fname))
-
-    line = dict()
-    line["SectorCount"] = int(descriptor[4])
-    line["RAMSize"] = int(descriptor[7], 0)
-    line["RAMStart"] = int(descriptor[6], 0)
-    line["RAMRange"] = (line["RAMStart"], line["RAMStart"] + line["RAMSize"] - 1)
-    line["FlashStart"] = int(descriptor[2], 0)
-    line["FlashSize"] = int(descriptor[3], 0)
-    line["FlashRange"] = (line["FlashStart"], line["FlashStart"] + line["FlashSize"] - 1)
-    line["RAMBufferOffset"] = int(descriptor[8], 0)
-    line["RAMStartWrite"] = line["RAMStart"] + line["RAMBufferOffset"]
     return line
